ee8013f0dbeef005c603f816d7515497616bb42f
WjPan1 <147006748+WjPan1@users.noreply.github.com>
2024-02-27 01:40:26 -0800

    added spinner to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,33 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { APP_NAME } from '../globals/globalVariables';
 
 import Banner from "../components/Banner";
 import CategoryRouter from "../router/CategoryRouter";
+import spinner from "../images/spinner.gif";
 
 import '../styles/Home.css';
 
 function Home () {
 
+    const [isLoading, setIsLoading] = useState(true)
+
     useEffect( () => {
         document.title = `${APP_NAME} - Home`;
+
+        setTimeout(() => {
+            setIsLoading(false);
+        }, 750);
+
     }, [])
+    
 
     return (
     <main className="home-main-container">
-        <Banner />
-        <CategoryRouter />
+            <img className={`spinner ${isLoading ? 'loading' : 'loaded'}`} src={spinner} alt="Spinner" />
+            <div className={`loading ${isLoading ? 'not-finish' : 'finished'}`}>
+                <Banner />
+                <CategoryRouter />
+            </div>
     </main>
     )
 }
